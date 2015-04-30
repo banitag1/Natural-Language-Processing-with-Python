7e65148f64a2334aea53cb16746ef73656fd72b0
Sequomics Research <SequomicsResearch@users.noreply.github.com>
2015-04-30 22:36:25 +0530

    author added

--- nlp/nlp.py
+++ nlp/nlp.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Author  :   Prabhat Kumar
+# Blog    :   http://prabhatkumar.org/
+# Created :   2015-04-30; 10:36:10
+
 __author__ = 'Prabhat Kumar'
